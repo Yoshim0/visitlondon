8be8b83be58311bd4629ef75f07597d203a0a1e4
Yoshimo <yoshimo@MacBook-Pro-2.local>
2018-08-07 21:38:16 +0200

    added copy html

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,20 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 
+// SOURCEFILES
 var sourcePaths ={
-	sassSource : 'src/scss/*.scss'
+	sassSource : 'src/scss/*.scss',
+	htmlSource : 'src/*.html'
 }
 
+// APP FILES
 var appPaths ={
 	root : 'app/',
 	css : 'app/css',
 	js : 'app/js'
 }
 
-// GULP TASK 
+// SASS COMPILER 
 gulp.task('sass', function(){
 	return gulp.src(sourcePaths.sassSource)
 		.pipe(autoprefixer())
@@ -22,6 +25,12 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest(appPaths.css));
 });
 
+gulp.task('copy', function(){
+	gulp.src(sourcePaths.htmlSource)
+		.pipe(gulp.dest(appPaths.root))
+});
+
+// BROWSERSYNC
 gulp.task('serve', ['sass'], function(){
 	browserSync.init([appPaths.css + '/*.css', appPaths.root + '/*.html', appPaths.js + '/*.js'],{
 		server: {
@@ -30,8 +39,11 @@ gulp.task('serve', ['sass'], function(){
 	})
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
+// GULP TASKS
+gulp.task('watch', ['serve', 'sass', 'copy'], function(){
 	gulp.watch([sourcePaths.sassSource], ['sass']);
+	gulp.watch([sourcePaths.htmlSource], ['copy']);
 });
 
+// GULP DEFAULT TASK
 gulp.task('default', ['watch']);
